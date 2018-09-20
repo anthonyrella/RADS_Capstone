7dd4fd02215a35fddbb0b6afecc6078af187216d
Anthony Rella <anthonyrella@outlook.com>
2018-09-19 21:02:06 -0400

    Added all intents and dialog delegate for FindRoom slot Duration

--- lambda/index.js
+++ lambda/index.js
@@ -4,21 +4,108 @@ const Alexa = require('ask-sdk');
 
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
-      return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
     },
     handle(handlerInput) {
-      const speechText = 'Welcome to Manage Meetings, what would you like to do?';
-  
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .reprompt(speechText)
-        .withSimpleCard('Hello World', speechText)
-        .getResponse();
-    }
-  };
+        const speechText = 'Welcome to Manage Meetings, what would you like to do?';
 
-  exports.handler = Alexa.SkillBuilders.custom()
-  .addRequestHandlers(
-    LaunchRequestHandler,
-   )
-  .lambda();
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .withSimpleCard('Hello World', speechText)
+            .getResponse();
+    }
+};
+
+const FindRoomHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'FindRoom';
+    },
+    handle(handlerInput) {
+
+        const updatedIntent = handlerInput.requestEnvelope.request.intent;
+        
+        if (handlerInput.requestEnvelope.request.dialogState != "COMPLETED") {
+            return handlerInput.responseBuilder
+                .addDelegateDirective()
+                .getResponse();
+                
+        } else {
+
+            var speechOutput = handlerInput.requestEnvelope.request.intent.slots.Duration.value;
+            return handlerInput.responseBuilder
+                .speak("The Time you've requested is " + speechOutput)
+                .reprompt(speechOutput)
+                .getResponse();
+        }
+    }
+};
+
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speechText = 'Say room finder to find and book a room';
+
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .withSimpleCard('Say room finder to find and book a room', speechText)
+            .getResponse();
+    }
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+                || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speechText = 'Goodbye!';
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withSimpleCard('Goodbye!', speechText)
+            .getResponse();
+    }
+};
+
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        //any cleanup logic goes here
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
+
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`Error handled: ${error.message}`);
+
+        return handlerInput.responseBuilder
+            .speak('Sorry, I can\'t understand the command. Please say again.')
+            .reprompt('Sorry, I can\'t understand the command. Please say again.')
+            .getResponse();
+    },
+};
+
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        FindRoomHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        SessionEndedRequestHandler)
+    .addErrorHandlers(ErrorHandler)
+    .lambda();
