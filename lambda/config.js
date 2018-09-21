f854984d33ce73b2a333d8340214ca3a1f1ef6c0
Raj <Raj@Rajs-Macbook-Pro.local>
2018-09-21 12:53:14 -0400

    Adding the config.js file back to the repo

---
+++ lambda/config.js
@@ -0,0 +1,12 @@
+/**
+ * @file Contains all config required to change rooms and skill.
+ */
+
+// App ID of Alexa Skill
+const appId = 'amzn1.ask.skill.1492be3f-007a-498f-938e-1fe051a33c3d';
+
+// Names of all meeting rooms to look for
+const testNames = ['First Room', 'Second Room', 'Third Room'];
+
+module.exports.appId = appId;
+module.exports.testNames = testNames;
