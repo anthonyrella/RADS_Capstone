ce3b5d92873633179bdeb937606077511d0d6768
Raj <Raj@Rajs-Macbook-Pro.local>
2018-09-24 17:39:57 -0400

    Resetting config.js to avoid merge conflicts

--- lambda/us-east-1_RoomFinder/config.js
+++ lambda/us-east-1_RoomFinder/config.js
@@ -5,10 +5,10 @@
  */
 
 // App ID of Alexa Skill
-const appId = 'amzn1.ask.skill.afb310c6-e27f-43b4-8128-e748d3e63772';
+const appId = 'amzn1.ask.skill.1492be3f-007a-498f-938e-1fe051a33c3d';
 
 // Names of all meeting rooms to look for
-const testNames = ['First Room', '12RS-04-10-03', 'Second Room', 'Third Room'];
+const testNames = ['First Room', 'Second Room', 'Third Room'];
 
 module.exports.appId = appId;
 module.exports.testNames = testNames;
